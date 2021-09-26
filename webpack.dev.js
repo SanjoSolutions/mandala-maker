9f69b597ddfa9f403c342d15543c5b4640302498
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-26 21:27:35 +0200

    Fixes webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,19 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { merge } from 'webpack-merge'
 import common from './webpack.common.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const config = merge(
   common,
   {
     mode: 'development',
+    devServer: {
+      static: {
+        directory: __dirname
+      }
+    }
   },
 )
 
